c836f1b4f17b38eee717e1be9e7562fad98ab01d
RaveMaker <ravemaker@msn.com>
2013-01-31 20:45:56 +0200

    "

---
+++ daily.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+echo "Starting Daily Backup"
+echo "Please Wait..."
+
+SNAPSHOT_RW=/backup;
+
+# step 1: delete the oldest snapshot, if it exists:
+if [ -d $SNAPSHOT_RW/daily.5 ] ;
+then
+	rm -rf $SNAPSHOT_RW/daily.5 ;
+fi ;
+	
+# step 2: shift the middle snapshots(s) back by one, if they exist
+if [ -d $SNAPSHOT_RW/daily.4 ] ;
+then
+	mv $SNAPSHOT_RW/daily.4 $SNAPSHOT_RW/daily.5 ;
+fi;
+if [ -d $SNAPSHOT_RW/daily.3 ] ;
+then
+	mv $SNAPSHOT_RW/daily.3 $SNAPSHOT_RW/daily.4 ;
+fi;
+if [ -d $SNAPSHOT_RW/daily.2 ] ;
+then
+	mv $SNAPSHOT_RW/daily.2 $SNAPSHOT_RW/daily.3 ;
+fi;
+if [ -d $SNAPSHOT_RW/daily.1 ] ;
+then
+	mv $SNAPSHOT_RW/daily.1 $SNAPSHOT_RW/daily.2 ;
+fi;
+
+# step 3: make a hard-link-only (except for dirs) copy of the latest snapshot, if that exists
+if [ -d $SNAPSHOT_RW/daily.0 ] ; 
+then
+	cp -al $SNAPSHOT_RW/daily.0 $SNAPSHOT_RW/daily.1 ;
+fi;
+	
+# step 4: rsync from the system into the latest snapshot (notice that
+# rsync behaves like cp --remove-destination by default, so the destination
+# is unlinked first.  If it were not so, this would copy over the other
+# snapshot(s) too!
+rsync -va --delete --delete-excluded --exclude-from '/backup/exclude.txt' /eng/ $SNAPSHOT_RW/daily.0 ;
+		    
+# step 5: update the mtime of daily.0 to reflect the snapshot time
+touch $SNAPSHOT_RW/daily.0 ;
+
+echo "All Done!"
---
+++ weekly.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+echo "Starting Weekly Backup"
+echo "Please Wait..."
+
+SNAPSHOT_RW=/backup;
+
+# step 1: delete the oldest snapshot, if it exists:
+if [ -d $SNAPSHOT_RW/weekly.3 ] ;
+then
+	rm -rf $SNAPSHOT_RW/weekly.3 ;
+fi ;
+
+# step 2: shift the middle snapshots(s) back by one, if they exist
+if [ -d $SNAPSHOT_RW/weekly.2 ] ;
+then
+    mv $SNAPSHOT_RW/weekly.2 $SNAPSHOT_RW/weekly.3 ;
+fi;
+if [ -d $SNAPSHOT_RW/weekly.1 ] ;
+then
+    mv $SNAPSHOT_RW/weekly.1 $SNAPSHOT_RW/weekly.2 ;
+fi;
+if [ -d $SNAPSHOT_RW/weekly.0 ] ;
+then
+    mv $SNAPSHOT_RW/weekly.0 $SNAPSHOT_RW/weekly.1;
+fi;
+
+# step 3: make a hard-link-only (except for dirs) copy of
+# daily.5, assuming that exists, into weekly.0
+if [ -d $SNAPSHOT_RW/daily.5 ] ;
+then
+    cp -al $SNAPSHOT_RW/daily.5 $SNAPSHOT_RW/weekly.0 ;
+fi;
+
+# note: do *not* update the mtime of weekly.0; it will reflect
+# when daily.5 was made, which should be correct.
+
+echo "All Done!"
