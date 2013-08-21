70c0d95f922f4e1d9ea6ec5af362f83983d0033a
Adam Mallul <RaveMaker@users.noreply.github.com>
2013-08-21 17:47:10 +0300

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -3,6 +3,7 @@ echo "Starting Daily Backup"
 echo "Please Wait..."
 
 SNAPSHOT_RW=/backup;
+SOURCEPATH=/home;
 
 # step 1: delete the oldest snapshot, if it exists:
 if [ -d $SNAPSHOT_RW/daily.5 ] ;
@@ -38,7 +39,7 @@ fi;
 # rsync behaves like cp --remove-destination by default, so the destination
 # is unlinked first.  If it were not so, this would copy over the other
 # snapshot(s) too!
-rsync -va --delete --delete-excluded --exclude-from '/backup/exclude.txt' /SOURCEPATH/ $SNAPSHOT_RW/daily.0 ;
+rsync -va --delete --delete-excluded --exclude-from '$SNAPSHOT_RW/exclude.txt' $SOURCEPATH $SNAPSHOT_RW/daily.0 ;
 		    
 # step 5: update the mtime of daily.0 to reflect the snapshot time
 touch $SNAPSHOT_RW/daily.0 ;
