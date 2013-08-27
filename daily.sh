31b8bc44c9e0175d51efa0e42dca73c89e44c888
RaveMaker <ravemaker@gmail.com>
2013-08-27 15:11:29 +0300

    Updated daily.sh to support N snapshots

--- daily.sh
+++ daily.sh
@@ -2,46 +2,52 @@
 
 echo "Starting Daily Backup Please Wait..."
 
-SNAPSHOT_RW=/backup;
-SOURCEPATH=/home;
+SNAPSHOT_RW="/backup"
+SOURCEPATH="/home"
+MAXSNAP=5
+CURRENTDIR=`pwd`
+
+# Move to backup location
+cd $SNAPSHOT_RW
 
 # step 1: delete the oldest snapshot, if it exists:
-if [ -d $SNAPSHOT_RW/daily.5 ] ;
+if [ -d daily.$MAXSNAP/ ]
 then
-	rm -rf $SNAPSHOT_RW/daily.5 ;
-fi;
-	
+	echo "Removing oldest backup copy"
+	rm -rf daily.$MAXSNAP/
+fi
+
 # step 2: shift the middle snapshots(s) back by one, if they exist
-if [ -d $SNAPSHOT_RW/daily.4 ] ;
-then
-	mv $SNAPSHOT_RW/daily.4 $SNAPSHOT_RW/daily.5 ;
-fi;
-if [ -d $SNAPSHOT_RW/daily.3 ] ;
-then
-	mv $SNAPSHOT_RW/daily.3 $SNAPSHOT_RW/daily.4 ;
-fi;
-if [ -d $SNAPSHOT_RW/daily.2 ] ;
-then
-	mv $SNAPSHOT_RW/daily.2 $SNAPSHOT_RW/daily.3 ;
-fi;
-if [ -d $SNAPSHOT_RW/daily.1 ] ;
-then
-	mv $SNAPSHOT_RW/daily.1 $SNAPSHOT_RW/daily.2 ;
-fi;
+let "MAXSNAP -= 1"
+while [  $MAXSNAP -ne "0" ]; do
+    if [ -d daily.$MAXSNAP/ ]
+    then
+	NEWSNAP=$(($MAXSNAP + 1))
+	echo "daily.$MAXSNAP moved to daily.$NEWSNAP"
+	mv daily.$MAXSNAP daily.$NEWSNAP
+	let "MAXSNAP -= 1"
+    else
+	echo "daily.$MAXSNAP not found"
+	let "MAXSNAP -= 1"
+    fi;
+done
 
 # step 3: make a hard-link-only (except for dirs) copy of the latest snapshot, if that exists
-if [ -d $SNAPSHOT_RW/daily.0 ] ; 
+if [ -d daily.0/ ]
 then
-	cp -al $SNAPSHOT_RW/daily.0 $SNAPSHOT_RW/daily.1 ;
+	echo "Make daily.1 a hard-link-only"
+	cp -al daily.0/ daily.1/
 fi;
-	
+
 # step 4: rsync from the system into the latest snapshot (notice that
 # rsync behaves like cp --remove-destination by default, so the destination
 # is unlinked first.  If it were not so, this would copy over the other
 # snapshot(s) too!
-rsync -va --delete --delete-excluded --exclude-from '$SNAPSHOT_RW/exclude.txt' $SOURCEPATH $SNAPSHOT_RW/daily.0 ;
-		    
-# step 5: update the mtime of daily.0 to reflect the snapshot time
-touch $SNAPSHOT_RW/daily.0 ;
+echo "Make daily.0 backup copy"
+rsync -va --delete --delete-excluded --exclude-from $CURRENTDIR/exclude.txt $SOURCEPATH $SNAPSHOT_RW/daily.0/
 
+# step 5: update the mtime of daily.0 to reflect the snapshot time
+touch daily.0/
+
+cd $CURRENTDIR
 echo "All Done!"
